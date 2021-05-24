236806ed32f81d16dc6a082be4441fcb19860fe7
Archit <archit.hadge@gmail.com>
2021-05-24 19:17:51 +0530

    Set up local dev environment for NodeJS and MySQL, added boilerplate code for basic NodeJS server.

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+var express=require('express');
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
