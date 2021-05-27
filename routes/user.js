0d2091947f174da09378ac168e73450062a8f03e
Archit <archit.hadge@gmail.com>
2021-05-27 17:52:10 +0530

    Added middleware for Auhtntication.

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 //Routes for user registration,login and getting users data
 const router=require('express').Router();
 const userController=require('../controllers/userController');
+const auth=require('../middlewares/auth');
 
 router.post('/login',userController.login);
 router.post('/register',userController.register);
-router.post('/registercomplaint',userController.registerComplaint);
+router.post('/registercomplaint',auth,userController.registerComplaint);
 
 
 module.exports=router;
--- server.js
+++ server.js
@@ -6,14 +6,7 @@ var app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/user',require('./routes/user'));
-// app.get('/',(req,res)=>{
-//     connection.query('SELECT Name FROM DEMO', function (error, results, fields) {
-//         console.log(results);
-//         res.json({
-//             results
-//         })
-//     });
-// })
+
 
 var server=app.listen(process.env.PORT,()=>{
     console.log("Server started at port ",process.env.PORT);
